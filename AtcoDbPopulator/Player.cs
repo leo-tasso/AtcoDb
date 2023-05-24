ad7b5c7f520cec939394e0e6d5865cf297c038f2
Tassinari Leonardo <leonardo.tassinari6@studio.unibo.it>
2023-05-24 02:26:04 +0200

    Change folder Structure

--- AtcoDbPopulator/Player.cs
+++ AtcoDbPopulator/Player.cs
@@ -1,7 +1,3 @@
-// <copyright file="Player.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
 namespace AtcoDbPopulator;
 using Models;
 
